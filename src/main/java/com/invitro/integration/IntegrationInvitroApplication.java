e2d93d8d32b58480c7ef3b1dcebb4c48029685c0
rifnur <rifnur69v@mail.ru>
2021-09-15 12:02:40 +0300

    Начало

---
+++ src/main/java/com/invitro/integration/IntegrationInvitroApplication.java
@@ -0,0 +1,13 @@
+package com.invitro.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationInvitroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationInvitroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/invitro/integration/IntegrationInvitroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invitro.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationInvitroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
